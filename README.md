3f969cbb4d874236d588fe83fc400534a5381ec4
Charles Jacquin <charles.jacquin@autistici.org>
2017-01-05 22:33:57 +0100

    starter

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Spotify React
+# Spotify --
 
 ## Installation
 ```
--- src/App/index.js
+++ src/App/index.js
@@ -10,20 +10,36 @@ class App extends PureComponent {
           <nav className="navbar navbar-default">
             <div className="container-fluid">
               <div className="navbar-header">
-                <a className='navbar-brand' href="#">Spotify --</a>
+                <a className="navbar-brand" href="#">Spotify --</a>
               </div>
               <ul className="nav navbar-nav navbar-right">
                 <li>
-                  <input className="form-control" type="search" placeholder="Search"/>
+                  <div className="Search">
+                    <div className="form-group">
+                      <input type="text" className="form-control" placeholder="Search" />
+                    </div>
+                  </div>
                 </li>
                 <li><a href="#">Signin</a></li>
               </ul>
             </div>
           </nav>
         </header>
-        <div className={styles.sidebar}>
+        <aside className={styles.sidebar}>
+          <div>
+            <h6 className={styles.title}>Ma musique</h6>
+            <a href="/" className={styles.link} >Tous</a>
+            <a href="/category/rock" className={styles.link}>Rock</a>
+            <a href="/category/pop" className={styles.link}>Pop</a>
+            <a href="/category/electro" className={styles.link}>Electro</a>
+            <a href="/category/latina" className={styles.link}>Latina</a>
+            <a href="/category/rap" className={styles.link}>Rap</a>
 
-        </div>
+            <h6 className={styles.title}>Playlist</h6>
+            <a className={styles.link} href="#">Ma musique</a>
+            <a className={styles.link} href="#">2016</a>
+          </div>
+        </aside>
         <div className={styles.main}>
           Hello world
         </div>
