4ab3c36d6c0d0f1ceb4056f7f3d87cd4531e2703
Charles Jacquin <charles.jacquin@autistici.org>
2017-01-09 23:08:40 +0100

    step2

--- src/index.js
+++ src/index.js
@@ -1,24 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { Route, Router, IndexRoute, browserHistory } from 'react-router'
-
 import App from 'containers/App'
-import Home from 'containers/Home'
-import SearchResult from 'containers/SearchResult'
-import Artist from 'containers/Artist'
-import store from 'store'
 
 const rootElem = document.getElementById('root')
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router history={browserHistory}>
-      <Route path="/" component={App}>
-        <IndexRoute component={Home} />
-        <Route path="/search/:query" component={SearchResult} />
-        <Route path="/artist/:id" component={Artist} />
-      </Route>
-    </Router>
-  </Provider>
+  <App />
   , rootElem)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,7 @@
 import { combineReducers } from 'redux'
-import artist from './artist'
-import album from './album'
-import player from './player'
 
 const rootReducer = combineReducers({
-  artist,
-  album,
-  player
+
 })
 
 export default rootReducer
